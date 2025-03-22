a2e299b43f0dc2c43c6e54a31cd5ddd51015850b
AlekseyAleksandrov <axcerz@gmail.com>
2025-03-22 11:39:17 +0300

    fix eslint

--- run.js
+++ run.js
@@ -1,7 +1,7 @@
 // Основной файл.
 // Запускает игру.
-const Game = require("./src/Game");
-const runInteractiveConsole = require("./src/keyboard");
+const Game = require('./src/Game');
+const runInteractiveConsole = require('./src/keyboard');
 
 // Инициализация игры с настройками.
 const game = new Game({
